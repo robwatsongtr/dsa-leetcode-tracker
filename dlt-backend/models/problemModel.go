4ad2582f26062ff241d4238869ba381eb4a43746
robwatsongtr <rwatso@gmail.com>
2024-07-12 19:05:17 -0700

    select all query now joins approach and difficulty tables

--- dlt-backend/models/problemModel.go
+++ dlt-backend/models/problemModel.go
@@ -5,18 +5,35 @@ import (
 )
 
 type Problem struct {
-	Id           int    `json:"id"`
-	Leetcode_num int    `json:"leetcode_num"`
-	Problem_name string `json:"problem_name"`
-	Description  string `json:"description"`
-	Approach_id  int    `json:"approach_id"`
-	Pseudocode   string `json:"pseudocode"`
-	Diff_id      int    `json:"diff_id"`
+	Id            int    `json:"id"`
+	Leetcode_num  int    `json:"leetcode_num"`
+	Problem_name  string `json:"problem_name"`
+	Description   string `json:"description"`
+	Approach_id   int    `json:"approach_id"`
+	Approach_name string `json:"approach_name"`
+	Pseudocode    string `json:"pseudocode"`
+	Diff_id       int    `json:"diff_id"`
+	Diff_level    string `json:"diff_level"`
 }
 
 func GetProblems(db *sql.DB) ([]Problem, error) {
 	rows, err := db.Query(
-		"SELECT id, leetcode_num, problem_name, description, approach_id, pseudocode, diff_id FROM problems")
+		`SELECT 
+			problems.id, 
+			problems.leetcode_num, 
+			problems.problem_name, 
+			problems.description, 
+			problems.approach_id, 
+			approach.approach_name,
+			problems.pseudocode, 
+			problems.diff_id, 
+			difficulty.diff_level
+		FROM problems
+		INNER JOIN approach ON problems.approach_id = approach.id
+		INNER JOIN difficulty ON problems.diff_id = difficulty.id 
+		ORDER BY id `,
+	)
+
 	if err != nil {
 		return nil, err
 	}
@@ -27,8 +44,17 @@ func GetProblems(db *sql.DB) ([]Problem, error) {
 
 	for rows.Next() {
 		var problem Problem
-		if err := rows.Scan(&problem.Id, &problem.Leetcode_num, &problem.Problem_name, &problem.Description,
-			&problem.Approach_id, &problem.Pseudocode, &problem.Diff_id); err != nil {
+		if err := rows.Scan(
+			&problem.Id,
+			&problem.Leetcode_num,
+			&problem.Problem_name,
+			&problem.Description,
+			&problem.Approach_id,
+			&problem.Approach_name,
+			&problem.Pseudocode,
+			&problem.Diff_id,
+			&problem.Diff_level,
+		); err != nil {
 			return nil, err
 		}
 		problems = append(problems, problem)
